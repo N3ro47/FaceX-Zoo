eb264d71840da122c79b5d58b53038cb9aefd75d
maj0r <kornelu123@github.com>
2025-03-26 16:54:47 +0100

    Adding working face recognition

--- face_sdk/api_usage/main.py
+++ face_sdk/api_usage/main.py
@@ -14,6 +14,11 @@ import torch
 
 from core.model_loader.face_detection.FaceDetModelLoader import FaceDetModelLoader
 from core.model_handler.face_detection.FaceDetModelHandler import FaceDetModelHandler
+from core.model_loader.face_alignment.FaceAlignModelLoader import FaceAlignModelLoader
+from core.model_handler.face_alignment.FaceAlignModelHandler import FaceAlignModelHandler
+from core.image_cropper.arcface_cropper.FaceRecImageCropper import FaceRecImageCropper
+from core.model_loader.face_recognition.FaceRecModelLoader import FaceRecModelLoader
+from core.model_handler.face_recognition.FaceRecModelHandler import FaceRecModelHandler
 
 cap=None
 name=None
@@ -21,6 +26,10 @@ if (sys.argv[-2] == "-v"):
     cap = cv2.VideoCapture(sys.argv[-1])
 elif (sys.argv[-1] == "-c"):
     cap = cv2.VideoCapture(0)
+elif (sys.argv[-1] == "help" or sys.argv[-1] == "-h"):
+    print("python3 main.py {-c (camera), -v path/to/mp4.mp4}")
+    print("During capture mode: \n q -> go to recognition mode\n p -> caputre a photo\n\nDuring recognition mode\nn -> next file in tmp dir\np -> previous file in tmp dir\nFiles in tmp are being captured during caputre mode\n")
+    sys.exit(-1)
 else:
     print("python3 main.py {-c (camera), -v path/to/mp4.mp4}")
     sys.exit(-1)
@@ -82,8 +91,6 @@ while True:
        logger.error('Face detection failed!')
        logger.error(e)
        sys.exit(-1)
-    else:
-       logger.info('Successful face detection!')
 
     bboxs = dets
 
@@ -93,7 +100,7 @@ while True:
         cv2.putText(frame, "{:0.2f}%".format(box[-1]*100), (_box[0] + 10, _box[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
     cv2.putText(frame, "Image counter: " + str(img_counter), (0,height - 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.imshow('Camera Feed', frame)
+    cv2.imshow('Capture mode', frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
@@ -116,6 +123,45 @@ if (fil_cap == 0):
     logger.error("No images in tmp folder")
     sys.exit(-1)
 
+model_category = 'face_alignment'
+model_name =  model_conf[scene][model_category]
+logger.info('Start to load the face landmark model...')
+try:
+    faceAlignModelLoader = FaceAlignModelLoader(model_path, model_category, model_name)
+    model, cfg = faceAlignModelLoader.load_model()
+    faceAlignModelHandler = FaceAlignModelHandler(model, 'cpu', cfg)
+except Exception as e:
+    logger.error('Failed to load face landmark model.')
+    logger.error(e)
+    sys.exit(-1)
+else:
+    logger.info('Success!')
+
+# face recognition model setting.
+model_category = 'face_recognition'
+model_name =  model_conf[scene][model_category]
+logger.info('Start to load the face recognition model...')
+try:
+    faceRecModelLoader = FaceRecModelLoader(model_path, model_category, model_name)
+    model, cfg = faceRecModelLoader.load_model()
+    faceRecModelHandler = FaceRecModelHandler(model, 'cpu', cfg)
+except Exception as e:
+    logger.error('Failed to load face recognition model.')
+    logger.error(e)
+    sys.exit(-1)
+else:
+    logger.info('Success!')
+
+face_cropper = FaceRecImageCropper()
+
+cap.release()
+cv2.destroyAllWindows()
+
+if (sys.argv[-2] == "-v"):
+    cap = cv2.VideoCapture(sys.argv[-1])
+elif (sys.argv[-1] == "-c"):
+    cap = cv2.VideoCapture(0)
+
 while True:
     ret, frame = cap.read()
     comb = np.concatenate((frame, cur_img), axis=1)
@@ -130,17 +176,38 @@ while True:
        logger.error('Face detection failed!')
        logger.error(e)
        sys.exit(-1)
-    else:
-       logger.info('Successful face detection!')
 
     bboxs = dets
 
-    for box in bboxs:
-        _box = list(map(int, box))
-        cv2.rectangle(frame, (_box[0], _box[1]), (_box[2], _box[3]), (0, 0, 255), 2)
-        cv2.putText(frame, "{:0.2f}%".format(box[-1]*100), (_box[0] + 10, _box[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    score=0
+    face_nums=0
+    try:
+        dets = faceDetModelHandler.inference_on_image(comb)
+        face_nums = dets.shape[0]
+        if face_nums != 2:
+            logger.info('Input image should contain two faces to compute similarity!')
+        feature_list = []
+        for i in range(face_nums):
+            landmarks = faceAlignModelHandler.inference_on_image(comb, dets[i])
+            landmarks_list = []
+            for (x, y) in landmarks.astype(np.int32):
+                landmarks_list.extend((x, y))
+            cropped_image = face_cropper.crop_image_by_mat(comb, landmarks_list)
+            feature = faceRecModelHandler.inference_on_image(cropped_image)
+            feature_list.append(feature)
+        score = np.dot(feature_list[0], feature_list[1])
+    except Exception as e:
+        logger.error('Pipeline failed!')
+        logger.error(e)
+        sys.exit(-1)
 
-    cv2.imshow('Camera Feed', comb)
+    if (face_nums == 2):
+        for box in bboxs:
+            _box = list(map(int, box))
+            cv2.rectangle(comb, (_box[0], _box[1]), (_box[2], _box[3]), (0, 0, 255), 2)
+            cv2.putText(comb, "Similarity: {:0.2f}%".format(score*100), (_box[0] + 10, _box[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow('Recognition mode', comb)
 
     key = cv2.waitKey(1) & 0xFF
 
